d13aa7c9bdf3721ceaff6785a6e2e3142bc56405
kdgnanbu <71370134+kdgnanbu@users.noreply.github.com>
2026-01-19 11:55:06 +0900

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/sample", (req, res) => {
+  res.send("hello");
+});
+
+const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
