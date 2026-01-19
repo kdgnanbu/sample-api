6e079a324ce0431e1ade81bb514c3815aefdd5a8
kdgnanbu <71370134+kdgnanbu@users.noreply.github.com>
2026-01-19 13:21:37 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// 全てのオリジンからのアクセスを許可（開発用）
+app.use(cors());
 
 app.get("/api/sample", (req, res) => {
   res.send("hello");
@@ -11,5 +15,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
